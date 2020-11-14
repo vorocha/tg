42de311ff9d43dab3b2be2e979e3710a4d2de473
Vinicius Rocha <vorocha@hotmail.com>
2020-11-14 09:27:49 -0300

    db basics

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('tipo');
+            $table->foreignId('profile_id')->constrained('profiles');
             $table->rememberToken();
             $table->timestamps();
         });
